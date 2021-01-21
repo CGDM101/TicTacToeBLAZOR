d4540b20cb40a2896c1424a2848c0e2de6798bef
CGDM101 <51097129+CGDM101@users.noreply.github.com>
2021-01-21 13:05:34 +0100

    Add project files.

---
+++ Models/Enums/PieceStyle.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TicTacToeBLAZOR.Models
+{
+    public enum PieceStyle
+    {
+        X,
+        O,
+        Blank
+    }
+}
